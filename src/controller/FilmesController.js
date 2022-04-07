7d9ec4abaaac2bf7d7f13dc138bcafda746414b8
Eduardo Nascimento <edwardo.xavier@gmail.com>
2022-04-07 02:44:13 -0300

    Update of code

--- src/controller/FilmesController.js
+++ src/controller/FilmesController.js
@@ -1,12 +1,16 @@
 import { variados } from '../model/filme.js';
 
 export const getIndex = async (req, res) => {
-  try {
-    const filmes = await variados.findAll();
-    res.render("index.ejs", {
-      filmes
-    });
+    try {
+      const filmes = await variados.findAll();
+      res.render("index.ejs", {
+        filmes
+      });
   } catch (err) {
-    console.log(err.message);
+    res.send(err.message);
   }
 };
+
+[{
+  
+}]
